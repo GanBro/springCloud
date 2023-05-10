ad9144155a10723ea3773e36533252e02205a951
wubo <2551921037@qq.com>
2023-05-10 23:40:51 +0800

    Gateway网关(跨域代码未写)

---
+++ gateway/src/main/java/cn/itcast/gateway/AuthorizeFilter.java
@@ -0,0 +1,39 @@
+package cn.itcast.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.lang.annotation.Annotation;
+
+@Order(-1)
+@Component
+public class AuthorizeFilter implements GlobalFilter, Order {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        MultiValueMap<String, String> params = request.getQueryParams();
+        String auth = params.getFirst("authorization");
+        if ("admin".equals(auth)) {
+            return chain.filter(exchange);
+        }
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int value() {
+        return -1;
+    }
+
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return null;
+    }
+}
